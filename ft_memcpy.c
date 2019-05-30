eb624c17de4dd1142928b954dd3481fbd1eaf206
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-05-30 14:33:29 +0200

    added ft_memcpy and test

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,25 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:29:09 by mcarter           #+#    #+#             */
-/*   Updated: 2019/05/22 12:29:11 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/05/30 13:24:50 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	
+	size_t	i;
+	char	*c_dst;
+	char	*c_src;
+
+	i = 0;
+	c_dst = (char *)dst;
+	c_src = (char *)src;
+	while (i < n)
+	{
+		c_dst[i] = c_src[i];
+		i++;
+	}
+	return (dst);
 }
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:42:43 by mcarter           #+#    #+#             */
-/*   Updated: 2019/05/30 12:17:29 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/05/30 14:27:42 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -232,6 +232,19 @@ int	main(void) //int argc, char** argv)
 	buf2 = ft_strdup("cihcohicohohch,'c'pg8298gph98gp908h");
 	ft_strdel(&buf2);
 	testfunc_s("ft_strdel", buf2, NULL);
+
+	ft_strncpy(buf, "test", 128);
+	ft_memcpy(buf, "abcdefghijklmnop", 8);
+	testfunc_s("ft_memcpy1", buf, "abcdefgh");
+	ft_memcpy(buf, "abcd", 128);
+	testfunc_s("ft_memcpy2", buf, "abcd");
+	buf_s = 0;
+	buf_i = 217;
+	ft_memcpy(&buf_s, &buf_i, sizeof(buf_s));
+	testfunc_i("ft_memcpy3", buf_s, 217);
+	buf2 = "\0 ";
+	ft_memcpy(&buf_s, buf2, sizeof(buf_s));
+	testfunc_i("ft_memcpy4", buf_s, 8192);
 	
 	return (0);
 }
