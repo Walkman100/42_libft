ea934ff5a5b6519186fcfca17f765dfc8e70881d
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-06-05 11:31:36 +0200

    added ft_lstdel and test

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 09:03:30 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/05 09:03:58 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/05 11:25:40 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
+	t_list	*current;
+	t_list	*tmp;
 
+	current = *alst;
+	while (current)
+	{
+		tmp = current;
+		current = current->next;
+		del(tmp->content, tmp->content_size);
+		free(tmp);
+		tmp = NULL;
+	}
+	*alst = NULL;
 }
--- tests/test_lstdel.c
+++ tests/test_lstdel.c
@@ -6,13 +6,26 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 09:11:35 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/05 09:11:58 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/05 11:29:25 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.h"
 
-void	test_lstdel(void)
+static void	lstdeltestdelegate(void *mem, size_t size)
 {
+	ft_memdel(&mem);
+	size++;
+}
 
+void		test_lstdel(void)
+{
+	t_list	*list;
+
+	list = ft_lstnew("1", 2);
+	list->next = ft_lstnew("2", 2);
+	list->next->next = ft_lstnew("3", 2);
+	ft_lstdel(&list, &lstdeltestdelegate);
+	print_list(list);
+	testfunc_s("ft_lstdel", (char *)list, NULL);
 }
