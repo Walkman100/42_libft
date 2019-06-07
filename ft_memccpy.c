ae45bfb97c33a27079df7de059020ec8995cddd8
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-06-07 15:21:08 +0200

    fixed ft_memccpy not copying with empty dst

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:29:18 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/06 12:40:30 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/07 14:41:28 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,24 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	size_t			i;
 	unsigned char	*c_dst;
 	unsigned char	*c_src;
+	int				cont;
 
 	i = 0;
 	c_dst = (unsigned char *)dst;
 	c_src = (unsigned char *)src;
-	while (i < n && c_src[i - 1] != (unsigned char)c)
+	cont = 1;
+	while (cont)
 	{
-		c_dst[i] = c_src[i];
-		i++;
+		if (i < n)
+		{
+			c_dst[i] = c_src[i];
+			i++;
+			if (i >= 0)
+				if (c_src[i - 1] == (unsigned char)c)
+					cont = 0;
+		}
+		else
+			cont = 0;
 	}
 	if (c_src[i - 1] == c)
 		return (dst + i);
