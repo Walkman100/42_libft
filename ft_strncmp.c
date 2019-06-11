b1fd12d2613273a6e4a2783681ab01ab00199c0f
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-06-11 09:59:23 +0200

    fixed ft_str(n)cmp not returning exactly the same as str(n)cmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:25:25 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/07 16:30:53 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/11 09:46:36 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
+	int		diff;
 
 	i = 0;
 	if (n == 0)
@@ -23,7 +24,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if (s1[i] - s2[i] != 0 || s1[i] == '\0' || i >= (n - 1))
 		{
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+			diff = ((unsigned char *)s1)[i] - ((unsigned char *)s2)[i];
+			if (diff < 0)
+				return (-1);
+			else if (diff > 0)
+				return (1);
+			return (diff);
 		}
 		i++;
 	}
--- tests/test_strcmp.c
+++ tests/test_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 10:02:48 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/07 16:29:19 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/11 09:41:08 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void	test_strcmp(void)
 	testfunc_i("ft_strcmp2", ft_strcmp("tesu", "test"), 1);
 	testfunc_i("ft_strcmp3", ft_strcmp("tess", "test"), -1);
 	testfunc_i("ft_strcmp4", ft_strcmp("abcdefghijklmn", "abcdefghijklmn"), 0);
-	testfunc_i("ft_strcmp5", ft_strcmp("TEST", "test"), -32);
-	testfunc_i("ft_strcmp6", ft_strcmp("testtest", "test"), 116);
+	testfunc_i("ft_strcmp5", ft_strcmp("TEST", "test"), -1);
+	testfunc_i("ft_strcmp6", ft_strcmp("testtest", "test"), 1);
 	testfunc_i("ft_strncmp1", ft_strncmp("test", "test", 50), 0);
 	testfunc_i("ft_strncmp2", ft_strncmp("tesu", "test", 3), 0);
-	testfunc_i("ft_strncmp3", ft_strncmp("TEST", "test", 2), -32);
+	testfunc_i("ft_strncmp3", ft_strncmp("TEST", "test", 2), -1);
 	testfunc_i("ft_strncmp4", ft_strncmp("testtest", "test", 4), 0);
-	testfunc_i("ft_strncmp5", ft_strncmp("testtest", "test", 500), 116);
+	testfunc_i("ft_strncmp5", ft_strncmp("testtest", "test", 500), 1);
 	testfunc_i("ft_strncmp6", ft_strncmp("zyxbcdefgh", "abcdwxyz", 0), 0);
 	testfunc_i("ft_strncmp6", ft_strncmp("abcdefgh", "", 0), 0);
 }
